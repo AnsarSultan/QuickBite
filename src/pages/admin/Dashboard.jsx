483d42a4addf8a8b20db01e7bb08f12b93c97130
Ansar Sultan <ansarsultan67@gmail.com>
2025-08-22 02:21:12 +0500

    Dashboard page completed

--- src/pages/admin/Dashboard.jsx
+++ src/pages/admin/Dashboard.jsx
@@ -8,14 +8,19 @@ function Dashboard() {
       <hr className="my-2 border-gray-300" />
       <div className="grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-4">
       <DashboardCard title="120" desciption="Total orders"/>  
-      <DashboardCard title="120" desciption="Orders in Proccess"/>  
-      <DashboardCard title="120" desciption="Orders to be Delivered"/>  
-      <DashboardCard title="120" desciption="Completed orders"/>  
-      <DashboardCard title="120" desciption="In-store orders"/>  
-      <DashboardCard title="120" desciption="Online orders"/>  
-      <DashboardCard title="120" desciption="Cancelled orders"/>  
-      <DashboardCard title="120" desciption="Today's sales"/>  
-     
+      <DashboardCard title="30" desciption="Orders in Proccess"/>  
+      <DashboardCard title="10" desciption="Orders to be Delivered"/>  
+      <DashboardCard title="80" desciption="Completed orders"/>  
+      <DashboardCard title="25" desciption="In-store pending orders"/>  
+      <DashboardCard title="4" desciption="Online pending orders"/>  
+      <DashboardCard title="2" desciption="Cancelled orders"/>  
+      <DashboardCard title="18,600" desciption="Today's sales"/>  
+      </div>
+      <h2 className="font-semibold text-xl mt-5">This month</h2>
+      <hr className="my-2 border-gray-300" />
+      <div className="grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-4">
+      <DashboardCard title="2300" desciption="Completed Orders" />
+      <DashboardCard title="25,0000" desciption="Sales" />
       </div>
     </div>
   );
--- src/pages/common/Orders.jsx
+++ src/pages/common/Orders.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 function Orders() {
   return (
-    <div>Orders</div>
+    <div className='h-full'>
+      <div>
+      <input type="text" className='bg-white border border-gray-500 p-2 rounded-lg'  placeholder='Enter orderID'/>
+      <button className='bg-admin text-white mx-3 px-3 py-1 rounded-xl cursor-pointer'>Search</button>
+      </div>
+    </div>
   )
 }
 
