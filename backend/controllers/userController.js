db70f0a7636f9e8864157b2ff004758243e195fd
Ansar Sultan <ansarsultan67@gmail.com>
2025-09-08 02:32:35 +0500

    Created admin add accounts API

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -77,4 +77,43 @@ const userLogin = async (req, res) => {
   
 };
 
-export { registerUser, userLogin };
+
+const addUserByAdmin  = async (req, res) => {
+  try {
+    const { name, email, password, phone, address , role} = req.body;
+    await Promise.all([
+    body("name").trim().notEmpty().withMessage("Name is required").run(req),
+    body("email").trim().notEmpty().withMessage("Email is required").isEmail().withMessage("Invalid Email").run(req),
+    body("password").trim().notEmpty().withMessage("Password is required").isLength({min: 8}).withMessage("Password must be 8 characters").run(req),
+    body("role").trim().notEmpty().withMessage("Please select a role").run(req)
+])
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+    }
+    const existingUser = await User.findOne({ where: { email } });
+    if (existingUser) {
+      return res
+        .status(400)
+        .json({ success: false, message: "Email already registered" });
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+    const newUser = await User.create({
+      name,
+      email,
+      password: hashedPassword,
+      phone,
+      address,
+      role
+    });
+    if (newUser) {
+      res.json({ success: true, message: `${role} account Added successfully` });
+    }
+  } catch (error) {
+    console.error("Error while registering user:", error);
+    res.status(500).json({ error: "Failed to create account" });
+  }
+};
+export { registerUser, userLogin , addUserByAdmin };
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,11 +1,13 @@
 import express from 'express';
-import  {registerUser, userLogin}  from '../controllers/userController.js';
+import  {addUserByAdmin, registerUser, userLogin}  from '../controllers/userController.js';
+import auth from '../middlewares/auth.js';
+import checkPermission from '../middlewares/accessControl.js';
 
 const userRouters = express.Router()
 
 userRouters.post('/register', registerUser)
 userRouters.post('/login', userLogin)
-
+userRouters.post('/addAccount' , auth , checkPermission("createAny" , "user") , addUserByAdmin)
 
 
 
