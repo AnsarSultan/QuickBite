709c556dec19909c3c87742af78c6b0ca98ac2d1
Ansar Sultan <ansarsultan67@gmail.com>
2025-09-15 23:14:30 +0500

    SHowing own orders list API is ready

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -1,102 +1,105 @@
 // import Order from "../models/Order.js";
 // import Order_item from "../models/Order_item.js";
 // import Product from "../models/Product.js";
-import { body , validationResult } from "express-validator";
+import { body, validationResult } from "express-validator";
 import Promotion from "../models/Promotion.js";
-import { Order, Order_item  , Product} from "../models/index.js";
+import { Order, Order_item, Product } from "../models/index.js";
 
-const placeOrder = async (req , res)=>{
-   try {
+const placeOrder = async (req, res) => {
+  try {
     const { id } = req.user
     function generateUniqueCode() {
-        const now = new Date();
-      
-        const day = String(now.getDate()).padStart(2, '0');
-        const month = String(now.getMonth() + 1).padStart(2, '0'); 
-        const year = now.getFullYear().toString().slice(-2);
-        const hours = String(now.getHours()).padStart(2, '0');
-        const minutes = String(now.getMinutes()).padStart(2, '0');
-      
-        const code = `${id}${year}${day}${month}${hours}${minutes}`;
-        return code;
-      }
-      
-      const order_tracking_id = generateUniqueCode()
+      const now = new Date();
 
+      const day = String(now.getDate()).padStart(2, '0');
+      const month = String(now.getMonth() + 1).padStart(2, '0');
+      const year = now.getFullYear().toString().slice(-2);
+      const hours = String(now.getHours()).padStart(2, '0');
+      const minutes = String(now.getMinutes()).padStart(2, '0');
 
-      const taken_by_id = id;
-    const { order_type , promotion_code , items} = req.body
-    if(!items || items.length === 0 ){
-        return res.status(404).json({success:false , message: "No items in order"})
+      const code = `${id}${year}${day}${month}${hours}${minutes}`;
+      return code;
+    }
+
+    const order_tracking_id = generateUniqueCode()
+
+    const userDetails = await User.findByPk(id);
+    //here i need to check if it is customer who is placing the order then i need to store cutomer id in the order
+    // table
+    
+    const taken_by_id = id;
+    const { order_type, promotion_code, items } = req.body
+    if (!items || items.length === 0) {
+      return res.status(404).json({ success: false, message: "No items in order" })
     }
 
     let total = 0;
     let orderItems = [];
-    for(let item of items){
-        const product = await Product.findByPk(item.product_id)
-        if(!product){
-            return res.status(404).json({success: false , message: "Product not found"})
-        }
-        const price = product.price;
-        const subtotal = price * item.quantity;
-        total += subtotal
+    for (let item of items) {
+      const product = await Product.findByPk(item.product_id)
+      if (!product) {
+        return res.status(404).json({ success: false, message: "Product not found" })
+      }
+      const price = product.price;
+      const subtotal = price * item.quantity;
+      total += subtotal
 
-        orderItems.push({
-            product_id: item.product_id,
-            quantity: item.quantity,
-            price,
-            subtotal
-        });
+      orderItems.push({
+        product_id: item.product_id,
+        quantity: item.quantity,
+        price,
+        subtotal
+      });
     }
 
 
     let discount = 0;
     let promotion_id = null;
-    if(promotion_code){
-        const promo = await Promotion.findOne({where: { code: promotion_code, is_active: true}})
-        if (
-            promo &&
-            new Date() >= new Date(promo.start_date) &&
-            new Date() <= new Date(promo.end_date)
-          ) {
-            promotion_id = promo.promotion_id;
-            if (promo.type === "percentage") {
-              discount = (total * promo.value) / 100;
-            } else if (promo.type === "flat") {
-              discount = promo.value;
-            }
-          } else {
-            return res.json({ success: false, message: "Promo code is not valid right now" });
-          }
-          
+    if (promotion_code) {
+      const promo = await Promotion.findOne({ where: { code: promotion_code, is_active: true } })
+      if (
+        promo &&
+        new Date() >= new Date(promo.start_date) &&
+        new Date() <= new Date(promo.end_date)
+      ) {
+        promotion_id = promo.promotion_id;
+        if (promo.type === "percentage") {
+          discount = (total * promo.value) / 100;
+        } else if (promo.type === "flat") {
+          discount = promo.value;
+        }
+      } else {
+        return res.json({ success: false, message: "Promo code is not valid right now" });
+      }
+
     }
 
     const finalTotal = total - discount
 
     const order = await Order.create({
-        order_uuid: order_tracking_id,
-        status: "pending",
-        payment_status: "paid",
-        order_type,
-        discount,
-        delivery_charges: 0,
-        promotion_id,
-        total_amount: finalTotal,
-        customer_id: null,
-        taken_by_id
+      order_uuid: order_tracking_id,
+      status: "pending",
+      payment_status: "paid",
+      order_type,
+      discount,
+      delivery_charges: 0,
+      promotion_id,
+      total_amount: finalTotal,
+      customer_id: null,
+      taken_by_id
     });
 
-    for(let item of orderItems){
-            await Order_item.create({...item, order_id: order.order_id})
+    for (let item of orderItems) {
+      await Order_item.create({ ...item, order_id: order.order_id })
     }
 
-    res.json({success:true , message: "Order Placed successfully"})
+    res.json({ success: true, message: "Order Placed successfully" })
 
 
-   } catch (error) {
+  } catch (error) {
     console.log(error)
-    res.status(500).json({success: false , message: "Something went wrong. Please try again later."})
-   }
+    res.status(500).json({ success: false, message: "Something went wrong. Please try again later." })
+  }
 }
 
 
@@ -110,7 +113,7 @@ const getAllOrders = async (req, res) => {
           include: [
             {
               model: Product,
-              attributes: ["name", "image_url"], 
+              attributes: ["name", "image_url"],
             },
           ],
         },
@@ -125,11 +128,11 @@ const getAllOrders = async (req, res) => {
   }
 };
 
-const searchOrder = async (req , res)=>{
+const searchOrder = async (req, res) => {
   try {
-    const {id} = req.params;
+    const { id } = req.params;
     const order = await Order.findOne({
-      where: { order_uuid: id  },
+      where: { order_uuid: id },
       include: [
         {
           model: Order_item,
@@ -137,46 +140,73 @@ const searchOrder = async (req , res)=>{
           include: [
             {
               model: Product,
-              attributes: ["name", "image_url"], 
+              attributes: ["name", "image_url"],
             },
           ],
         }
       ]
     });
-    if(!order){
-      return res.status(404).json({success:false , message: "Record not found"})
+    if (!order) {
+      return res.status(404).json({ success: false, message: "Record not found" })
     }
-    return res.json({success: true , order: order })
+    return res.json({ success: true, order: order })
   } catch (error) {
     console.error(error);
     res.status(500).json({ success: false, message: "Something went wrong. Please try again later." });
   }
 }
 
-const updateOrderStatus = async (req , res)=>{
+const updateOrderStatus = async (req, res) => {
   try {
-    const {id} = req.params;
-    const {status} = req.body;
+    const { id } = req.params;
+    const { status } = req.body;
     await Promise.all([
-      body("status").isIn(['ready' , 'delivered']).run(req)
+      body("status").isIn(['ready', 'delivered']).run(req)
     ])
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
     const checkOrder = await Order.findByPk(id);
-    if(!checkOrder){
-      res.json({success:false , message: "Record not found."})
+    if (!checkOrder) {
+      res.json({ success: false, message: "Record not found." })
     }
     checkOrder.status = status;
     await checkOrder.save();
-    res.json({success:true , message: "Status Updated."})
+    res.json({ success: true, message: "Status Updated." })
   } catch (error) {
     console.log(error)
-    res.status(500).json({success:false , message: "Something went wront. Please try again later"})
+    res.status(500).json({ success: false, message: "Something went wront. Please try again later" })
   }
 
 }
 
+const getUserOrders = async (req , res)=>{
+  try {
+    const {id} = req.user;
+    const orders = await Order.findAll({
+      where: { taken_by_id: id},
+      include: [
+        {
+          model: Order_item,
+          attributes: ["order_item_id", "product_id", "quantity", "price", "subtotal"],
+          include: [
+            {
+              model: Product,
+              attributes: ["name", "image_url"],
+            },
+          ],
+        },
+      ],
+      order: [["createdAt", "DESC"]],
+    });
 
-export {placeOrder , getAllOrders , searchOrder , updateOrderStatus}
+    res.json({ success: true, data: orders });
+  } catch (error) {
+    console.log(error)
+    res.status(500).json({ success: false, message: "Something went wront. Please try again later" })
+  }
+}
+
+
+export { placeOrder, getAllOrders, searchOrder, updateOrderStatus , getUserOrders}
--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,13 +1,14 @@
 import express from "express";
 import auth from "../middlewares/auth.js";
 import checkPermission from "../middlewares/accessControl.js";
-import { placeOrder, getAllOrders , searchOrder, updateOrderStatus } from "../controllers/orderController.js";
+import { placeOrder, getAllOrders , searchOrder, updateOrderStatus, getUserOrders } from "../controllers/orderController.js";
 
 
 const orderRouter = express.Router();
 
 orderRouter.get('/', auth , checkPermission('readAny', 'order'), getAllOrders)
 orderRouter.get('/:id' , auth , checkPermission('readAny' , 'order'), searchOrder)
+orderRouter.get('/my/orders' , auth , checkPermission('readOwn' , 'order'), getUserOrders)
 
 orderRouter.post('/' , auth , checkPermission("createOwn" , "order") , placeOrder)
 orderRouter.patch('/:id' , auth , checkPermission('updateAny' , "order") , updateOrderStatus)
