a4faff6c6c4180c82d5714103cdab57ab14ac9f0
Ansar Sultan <ansarsultan67@gmail.com>
2025-09-17 23:56:06 +0500

    created navbar for the customer's Layout

--- frontend/src/layouts/CustomerLayout.jsx
+++ frontend/src/layouts/CustomerLayout.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
+import Navbar from "../components/customer/Navbar";
 
 function CustomerLayout() {
   return (
     <div className="flex w-full bg-stone-100 h-screen">
-      {/* <Sidebar /> */}
+      <Navbar/>
       <h2>customer Layout</h2>
       <div className="flex-1 p-4 overflow-y-auto">
         <Outlet />
