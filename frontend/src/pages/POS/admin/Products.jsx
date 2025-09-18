026ae830486ce267b3c3dc77abed73e0ca11f690
Ansar Sultan <ansarsultan67@gmail.com>
2025-09-19 00:16:20 +0500

    working on products page...

--- frontend/src/pages/POS/admin/Products.jsx
+++ frontend/src/pages/POS/admin/Products.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import { Plus } from 'lucide-react';
 
 function Products() {
   return (
-    <div>Products</div>
+    <div className='h-screen'>
+      <div className='flex gap-2 items-center'>
+        <div className='flex bg-admin text-white p-2 rounded'> <Plus />Add new product</div>
+        <div className='flex bg-admin text-white p-2 rounded'> <Plus />Add new category</div>
+      </div>
+    </div>
   )
 }
 
