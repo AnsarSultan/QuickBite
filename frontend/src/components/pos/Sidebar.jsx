ee7849500bd068e42c966038b3057f0305d21124
Ansar Sultan <ansarsultan67@gmail.com>
2025-09-19 17:53:42 +0500

    change the sidebar css CLasses to role based theme

--- frontend/src/components/pos/Sidebar.jsx
+++ frontend/src/components/pos/Sidebar.jsx
@@ -3,17 +3,20 @@ import { NavLink } from 'react-router-dom'
 import logo from '../../assets/logo.png'
 
 function Sidebar() {
+  const role= "admin";
+  const acitveClasses = `bg-${role} text-white shadow-md`;
+  const nonActiveClasses = `bg-stone-100 hover:bg-H${role}`
     const navlinkClasses = "w-full h-11 flex items-center justify-center my-2 rounded text-xl"
   return (
     <div className='w-48 bg-white flex flex-col items-center shadow-xl p-4'>
         <img className='w-24' src={logo} alt="" />
-        <NavLink to="/pos/dashboard" className={({isActive})=> `${navlinkClasses} ${isActive ? "bg-admin text-white shadow-md" : "bg-stone-100 hover:bg-Hadmin"}`}>Dashboard</NavLink>
-        <NavLink to="/pos/orders" className={({isActive})=> `${navlinkClasses} ${isActive ? "bg-admin text-white shadow-md" : "bg-stone-100 hover:bg-Hadmin" }`}>Orders</NavLink>
-        <NavLink to="/pos" end className={({isActive})=> `${navlinkClasses} ${isActive ? "bg-admin text-white shadow-md" : "bg-stone-100 hover:bg-Hadmin" }`}>POS</NavLink>
-        <NavLink to="/pos/products" className={({isActive})=> `${navlinkClasses} ${isActive ? "bg-admin text-white shadow-md" : "bg-stone-100 hover:bg-Hadmin" }`}>Products</NavLink>
-        <NavLink to="/pos/reports" className={({isActive})=> `${navlinkClasses} ${isActive ? "bg-admin text-white shadow-md" : "bg-stone-100 hover:bg-Hadmin" }`}>Reports</NavLink>
-        <NavLink to="/pos/users" className={({isActive})=> `${navlinkClasses} ${isActive ? "bg-admin text-white shadow-md" : "bg-stone-100 hover:bg-Hadmin" }`}>Users</NavLink>
-        <NavLink to="/pos/settings" className={({isActive})=> `${navlinkClasses} ${isActive ? "bg-admin text-white shadow-md" : "bg-stone-100 hover:bg-Hadmin" }`}>Settings</NavLink>
+        <NavLink to="/pos/dashboard" className={({isActive})=> `${navlinkClasses} ${isActive ? `${acitveClasses}` : `${nonActiveClasses}`}`}>Dashboard</NavLink>
+        <NavLink to="/pos/orders" className={({isActive})=> `${navlinkClasses} ${isActive ? `${acitveClasses}` : `${nonActiveClasses}` }`}>Orders</NavLink>
+        <NavLink to="/pos" end className={({isActive})=> `${navlinkClasses} ${isActive ? `${acitveClasses}` : `${nonActiveClasses}` }`}>POS</NavLink>
+        <NavLink to="/pos/products" className={({isActive})=> `${navlinkClasses} ${isActive ? `${acitveClasses}` : `${nonActiveClasses}` }`}>Products</NavLink>
+        <NavLink to="/pos/reports" className={({isActive})=> `${navlinkClasses} ${isActive ? `${acitveClasses}` : `${nonActiveClasses}` }`}>Reports</NavLink>
+        <NavLink to="/pos/users" className={({isActive})=> `${navlinkClasses} ${isActive ? `${acitveClasses}` : `${nonActiveClasses}` }`}>Users</NavLink>
+        <NavLink to="/pos/settings" className={({isActive})=> `${navlinkClasses} ${isActive ? `${acitveClasses}` : `${nonActiveClasses}` }`}>Settings</NavLink>
         <button className="w-full h-11 flex items-center justify-center my-2 rounded text-xl cursor-pointer bg-red-500 text-white hover:shadow-xl">Logout</button>
     </div>
   )
